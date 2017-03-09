904ecbc7956e3bce5ce715763036a227ca94bd01
Patrick Lising <lising.patrick@gmail.com>
2017-03-10 02:36:09 +0800

    Added SplashScreen, Changed LauncherActivity on Manifest, Added Style,Theme,Color

---
+++ app/src/main/java/com/callofnature/poopste/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.callofnature.poopste;
+
+import android.content.Intent;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
